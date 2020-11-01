0ee24268acb5ba44cda50dabe11ed69e6155f5e8
Nikita Shitov <shitovn16@gmail.com>
2020-11-01 19:35:30 +0500

    Add two first blocks and create some structure

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="description" content="Furniture E-commerce">
+    <meta name="keywords" content="IKEA, Furniture, Hygge, Danish, ">
+    <meta name="author" content="Shitov and Saitgareeva">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" href="styles/bootstrap-grid.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Hygge</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="row d-flex">
+                <div class="header-icons col-12 d-flex justify-content-between align-items-center">
+                    <img src="assets/img/search.svg" alt="Search icon" class="header-icons__search" width="45" height="45">
+                    <img src="assets/img/logotype.svg" alt="Logotype Hygge" class="header-icons__logo" width="251" height="121">
+                    <img src="assets/img/bag.svg" alt="Bag icon" class="header-icons__bag" width="45" height="45">
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <nav class="header-nav col-8">
+                    <ul class="header-nav__ul flex-row d-flex justify-content-between">
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="">Catalog</a>
+                        </li>
+                        <li>
+                            <a href="">Blog</a>
+                        </li>
+                        <li>
+                            <a href="">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="">Support</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="container-fluid no-gutters lyrics">
+            <div class="row">
+                <video autoplay muted loop>
+                    <source src="assets/Hygge_Video.mp4" type="video/mp4">
+                </video>
+                <div class="lyrics-text col-5 d-flex justify-content-center flex-column">
+                    <h2>We create things that create a cozy atmosphere</h2>
+                    <button>Catalog</button>
+                </div>
+            </div>
+        </div>
+    </header>
+    <script src="scripts/script.js"></script>
+</body>
+</html>
